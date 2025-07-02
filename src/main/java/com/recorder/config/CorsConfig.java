6b3f432f1ca0b08995e918013d92bfc456ab5b04
Janderson <jandersonjunio@hotmail.com>
2025-07-02 16:44:13 -0300

    second commit

--- src/main/java/com/recorder/config/CorsConfig.java
+++ src/main/java/com/recorder/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5000")
+                        .allowedOrigins("http://127.0.0.1:5000", "null", "http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
